b8715b66038d5dc9e44a027a68b66ed55216e1e7
Imad saad <72302093+imadsaad12@users.noreply.github.com>
2024-02-15 21:43:53 +0200

    added skeleton loader

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function App() {
   return (
--- src/pages/viewproducts/skeleteon/styles.js
+++ src/pages/viewproducts/skeleteon/styles.js
@@ -1,12 +1,15 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
 import { breakingPoints } from "../../../global/breakingPoints";
 
 export const Container = styled.div`
   width: 25%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  height: 410px;
   margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
   @media (max-width: ${breakingPoints.md}px) {
     width: 50%;
   }
@@ -23,15 +26,11 @@ export const Wrapper = styled.div`
   gap: 3px;
 `;
 
-export const Image = styled.img`
-  width: 100%;
-  height: 50vh;
-`;
-export const Name = styled.span`
-  font-size: 15px;
-  font-weight: bold;
-`;
-export const Price = styled.span`
-  font-size: 15px;
-  font-weight: normal;
+export const ColorSkeletonContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 10px;
 `;
